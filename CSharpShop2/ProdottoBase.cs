e12dcc71e15caa5b9101f832353a56961bb02ffd
Pedra96 <darkraidialga@yahoo.it>
2022-12-01 14:45:23 +0100

    creazione classe base

---
+++ CSharpShop2/ProdottoBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop2 {
+    public class ProdottoBase {
+       private string nome;
+       int prezzo;
+       double peso;
+       double iva;
+       string descrizione;
+        public ProdottoBase(string nome,int prezzo,double peso,double iva,string descrizione="") {
+            this.nome = nome;
+            this.prezzo = prezzo;
+            this.peso = peso;
+            this.iva = iva;
+            this.descrizione= descrizione;
+        }
+        public string GetNome() {
+            return nome;
+        }
+        public int GetPrezzo() {
+            return prezzo;
+        }
+        public double GetPeso() {
+            return peso;
+        }
+        public double GetIva() {
+            return iva;
+        }
+        public string GetDescrizione() {
+            return descrizione;
+        }
+    }
+}
--- CSharpShop2/Program.cs
+++ CSharpShop2/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharpShop2;
+
 Console.WriteLine("Hello, World!");
+
+var ProdottoBase= new Elettrodomestico();
+
+ProdottoBase.setName("test");
+Console.WriteLine(ProdottoBase.getNome());
